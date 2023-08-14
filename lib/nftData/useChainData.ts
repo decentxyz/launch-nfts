cc974f4b41629f49a68770eb8809259a103036be
cdsiren <charlie@decent.xyz>
2023-08-14 18:24:11 -0400

    test

--- lib/nftData/useChainData.ts
+++ lib/nftData/useChainData.ts
@@ -6,8 +6,6 @@ export const useChainData = (date?: string) => {
   const fetcher = (url: string) => axios.get(url).then(res => res.data);
   const { data, isLoading, error } = useSWR(`${absoluteUrl()?.origin}/api/getChainData?targetDate=${date}`, fetcher);
 
-  console.log(data)
-
   return {
     chainData: data,
     loadingChainData: isLoading,
