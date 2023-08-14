ae28c85e544f7bd4df4e0fe02f6a156cf3c11b83
cdsiren <charlie@decent.xyz>
2023-08-14 19:18:05 -0400

    test

--- lib/nftData/getContractData.ts
+++ lib/nftData/getContractData.ts
@@ -1,28 +1,21 @@
 import axios from 'axios';
 import { Address } from 'viem';
 
-const cache = new Map();
-
 export const getContractData = async (addresses: Address[]) => {
-  if (cache.has(addresses)) {
-    return cache.get(addresses);
-  }
-
   try {
-    const response = await axios.get("https://api-base.reservoir.tools/collections/v6", {
-      headers: {
-        "x-api-key": process.env.RESERVOIR_API_KEY,
-      },
-      params: {
-        contract: addresses,
-      },
-    });
+    const { data: contractData } = await axios.get(
+      "https://api-base.reservoir.tools/collections/v6", {
+        headers: {
+          "x-api-key": process.env.RESERVOIR_API_KEY as string,
+        },
+        params: {
+          contract: addresses
+        }
+      });
 
-    const contractData = response.data.collections;
-    cache.set(addresses, contractData);
-    return contractData;
-  } catch (error) {
-    console.error(error);
-    throw new Error('Error getting NFT data.');
-  }
-};
+    return contractData.collections;
+  } catch (e) {
+    console.error(e);
+    throw new Error('Error getting nft data.');
+  };
+}
--- pages/mint/[address].tsx
+++ pages/mint/[address].tsx
@@ -16,16 +16,26 @@ import { parseUnits } from "viem";
 const Mint: NextPage = (props: any) => {
   const {
     query: { address },
-    contractData
+    // contractData
   } = props;
   const { address: account } = useAccount();
   const [activeTab, setActiveTab] = useState('Mint');
   const [mintInfo, setMintInfo] = useState<MintInfo>();
+  const [contractData, setContractData] = useState<any>()
 
   useEffect(() => {
     const data = getMintInfo('0x05b8ee5658f3ad6c268c08b7a70f2fb4b10cf348', 1, account);
-    setMintInfo(data)
+    setMintInfo(data);
   }, [account]);
+
+  useEffect(() => {
+    async function load() {
+      const data = await getContractData([address]);
+      setContractData(data)
+    }
+    load()
+  }, [address]);
+  if (contractData) console.log("Heeeea", contractData)
 
   return (
     <>
@@ -33,16 +43,16 @@ const Mint: NextPage = (props: any) => {
     <div className={`${styles.main} px-[24px] py-[12px] relative`}>
       <div className='flex md:flex-wrap flex-wrap-reverse md:gap-0 gap-12 md:mt-0 mt-40 w-full relative'>
         <div className='md:w-1/2 w-full h-full pr-8 relative'>
-          <p className="font-thin text-7xl">{contractData[0].name}</p>
+          {/* <p className="font-thin text-7xl">{contractData[0].name}</p>
 
           <div className='pt-10 mb-2 md:w-[500px] border-b border-black flex justify-center'>
             <div className='pb-2 flex text-xl'>
               <button onClick={() => setActiveTab('Mint')} className={`${activeTab !== 'Mint' && 'text-gray-500 font-thin'} pr-16 border-r border-black hover:text-opacity-80`}>Mint</button>
               <button onClick={() => setActiveTab('Details')} className={`${activeTab !== 'Details' && 'text-gray-500 font-thin'} pl-16 hover:text-opacity-80`}>Details</button>
             </div>
-          </div>
+          </div> */}
           <div>
-            {activeTab === 'Mint' && mintInfo ? <>
+            {activeTab === 'Mint' && mintInfo && contractData ? <>
               <TheBox
                 className="text-xs md:max-w-[500px] bg-white bg-opacity-50"
                 paymentButtonText="Pay now"
@@ -60,23 +70,26 @@ const Mint: NextPage = (props: any) => {
                 apiKey='105a75d8ff0a9ee14015ba4c30f9d550'
               />
             </> : <>
-              <div className='flex items-center md:w-[500px] justify-between flex-wrap gap-2 text-sm font-thin py-4'>
+              {/* <div className='flex items-center md:w-[500px] justify-between flex-wrap gap-2 text-sm font-thin py-4'>
                 <p>Mint start: {convertTimestamp(mintInfo?.startDate)}</p>
                 <p>Mint end: {convertTimestamp(mintInfo?.endDate) || 'Open'}</p>
                 <p>Max tokens: {mintInfo?.maxTokens || 'Open'}</p>
                 <Link target="_blank" className='flex gap-2' href={`https://basescan.org/address/${contractData[0].primaryContract || ''}`}>{BaseScan(18, 20)} <span className='underline'> View on Basescan</span></Link> 
               </div>
-              <p className='mt-8 md:w-[500px] overflow-y-auto max-h-[250px]'>{contractData[0].description}</p>
+              <p className='mt-8 md:w-[500px] overflow-y-auto max-h-[250px]'>{contractData[0].description}</p> */}
             </>
+            }
+            {
+            contractData && JSON.stringify(contractData)
             }
           </div>
         </div>
 
-        <div className='md:w-1/2 w-full flex justify-center'>
+        {/* <div className='md:w-1/2 w-full flex justify-center'>
           <Image src={contractData[0].image} height={500} width={500} alt="nft image" className='rounded-md' />
-        </div>
+        </div> */}
       </div>
-      <MintFooter contractData={contractData} />
+      {/* <MintFooter contractData={contractData} /> */}
     </div>
     </>
   )
@@ -87,26 +100,26 @@ export default Mint;
 export const getServerSideProps = async (context: any) => {
   const { address } = context.query;
 
-  try {
-    let nftData = null;
+  // try {
+  //   let nftData = null;
 
-    if (address) {
-      nftData = await getContractData([address]);
-    }
+  //   if (address) {
+  //     nftData = await getContractData([address]);
+  //   }
 
+  //   return {
+  //     props: {
+  //       contractData: nftData,
+  //       query: context.query,
+  //     },
+  //   };
+  // } catch (error) {
+  //   console.error(error);
     return {
       props: {
-        contractData: nftData,
+        // contractData: null,
         query: context.query,
       },
     };
-  } catch (error) {
-    console.error(error);
-    return {
-      props: {
-        contractData: null,
-        query: context.query,
-      },
-    };
-  }
+  // }
 };
