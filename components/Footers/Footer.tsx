369363980f537562bdf565c37e377dcc13d3a5e4
Charlie Durbin <charlie@decent.xyz>
2024-05-23 09:07:49 -0700

    sm update

--- components/Footers/Footer.tsx
+++ components/Footers/Footer.tsx
@@ -23,7 +23,7 @@ const Footer = (props: any) => {
             {parseInt(activeNft?.tokenCount).toLocaleString()}
           </p>
           <p className="w-1/4 hidden sm:inline-block">
-            {((activeNft?.ownerCount / parseInt(activeNft?.tokenCount))*100).toFixed(2)+ "%"}
+            {(((activeNft?.ownerCount - 1) / parseInt(activeNft?.tokenCount))*100).toFixed(2)+ "%"}
           </p>
           <p className="w-1/4 text-right hidden sm:inline-block">
             {((parseInt(activeNft?.onSaleCount) / parseInt(activeNft?.tokenCount))*100).toFixed(2)+ "%"}
